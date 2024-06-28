ebb3ca6f9463461ece10e10cee9efa7fdff6ffba
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-28 16:52:59 -0300

    create: standard classes

---
+++ sys-touch/src/main/java/com/systouch/SysTouchApplication.java
@@ -0,0 +1,13 @@
+package com.systouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysTouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysTouchApplication.class, args);
+	}
+
+}
