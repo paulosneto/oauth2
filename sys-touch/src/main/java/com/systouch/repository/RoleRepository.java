d6364d94eb19f2fa75cc7b936d46f3a198fd37e0
paulo_sneto <paulo_sneto@yahoo.com>
2024-06-28 16:57:25 -0300

    create: created RoleRepository class

---
+++ sys-touch/src/main/java/com/systouch/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.systouch.repository;
+
+import com.systouch.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
