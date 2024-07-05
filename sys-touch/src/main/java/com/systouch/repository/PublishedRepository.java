b2a58f3254466d9a0a8392fadd7f2d3c8d0ae43c
paulo_sneto <paulo_sneto@yahoo.com>
2024-07-05 15:09:13 -0300

    fix: modified PublishedRepository.java

--- sys-touch/src/main/java/com/systouch/repository/PublishedRepository.java
+++ sys-touch/src/main/java/com/systouch/repository/PublishedRepository.java
@@ -1,7 +1,10 @@
 package com.systouch.repository;
 
 import com.systouch.domain.Published;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublishedRepository extends JpaRepository<Published, Long> {
+
+
 }
